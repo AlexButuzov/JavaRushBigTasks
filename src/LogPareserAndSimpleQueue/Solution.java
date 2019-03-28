7608343119c1f294acc094cbf7b8f3f91af62a26
AlexButuzov <butuzov.ne@gmail.com>
2019-03-28 14:29:29 +0300

    LogPareserAndSimpleQueue утранение ошибки в адресации ресурса

--- src/LogPareserAndSimpleQueue/Solution.java
+++ src/LogPareserAndSimpleQueue/Solution.java
@@ -3,17 +3,21 @@ package LogPareserAndSimpleQueue;
 import java.nio.file.Paths;
 
 public class Solution {
+    private static String sourceAdress = (Solution.class.getProtectionDomain().getCodeSource().getLocation().getPath().substring(1) +
+            Solution.class.getPackage().getName().replaceAll(".model", "") + ".logs")
+            .replaceAll("[.]", "/").replace("target/classes", "src");
     public static void main(String[] args) {
-        LogParser logParser = new LogParser(Paths.get("C:\\_nout\\java\\_projects\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\LogPareserAndSimpleQueue\\logs"));
+        //System.out.println(sourceAdress);
+        LogParser logParser = new LogParser(Paths.get(sourceAdress));
        // System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
         //System.out.println(logParser.getUniqueIPs(null, null));
        // System.out.println(logParser.getIPsForEvent(Event.DONE_TASK, null, null));
        // System.out.println(logParser.getIPsForStatus(Status.FAILED, null, null));
        // System.out.println(logParser.getIPsForUser("Amigo",null,null));
-        //System.out.println(logParser.getNumberOfUserEvents("Amigo",null,null));
-        //System.out.println(logParser.getAllSolvedTasksAndTheirNumber(null,null));
-        //System.out.println(logParser.getAllDoneTasksAndTheirNumber(null,null));
-       // System.out.println(logParser.execute("get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\""));
+        System.out.println(logParser.getNumberOfUserEvents("Amigo",null,null));
+        System.out.println(logParser.getAllSolvedTasksAndTheirNumber(null,null));
+        System.out.println(logParser.getAllDoneTasksAndTheirNumber(null,null));
+        System.out.println(logParser.execute("get ip for user = \"Eduard Petrovich Morozko\" and date between \"11.12.2013 0:00:00\" and \"03.01.2014 23:59:59\""));
 
 
         System.out.println(logParser.execute("get ip for user = \"Amigo\" and date between \"11.12.2013 0:00:00\" and \"03.01.2046 23:59:59\""));
